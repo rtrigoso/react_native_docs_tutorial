438468c3750b16d06a5a5ac947e4505a2db83268
ren trigoso <renzotrigoso@gmail.com>
2019-08-18 15:43:55 -0400

    added fetch example to listexamples component

--- components/ListExamples.js
+++ components/ListExamples.js
@@ -20,37 +20,41 @@ const styles = StyleSheet.create({
 })
 
 export default class ListExamples extends Component {
-    _onPress() {
-        alert('button Pressed!');
+    constructor(props)
+    {
+        super(props);
+        this.state = {movies: []}
     }
+
+    async getMovies()
+    {
+        try
+        {
+            let response = await fetch('https://facebook.github.io/react-native/movies.json')
+            let responseJson = await response.json();
+            let tmpMovies = responseJson.movies.map(movie => {
+                return {'key': movie.title}
+            });
+            this.setState({movies: tmpMovies})
+        }
+        catch(err)
+        {
+            console.error(err);
+        }
+    }
+    
+    componentDidMount()
+    {
+        this.getMovies();
+    }
+
     render() {
         return (
             <View style={{height: 250}}>
                 <View style={{flex:1, flexDirection: 'row'}}> 
                     <View style={{flex: 1}}>
                         <FlatList
-                            data={[
-                                {key: 'Devin'},
-                                {key: 'Dan'},
-                                {key: 'Dominic'},
-                                {key: 'Jackson'},
-                                {key: 'James'},
-                                {key: 'Joel'},
-                                {key: 'John'},
-                                {key: 'Jillian'},
-                                {key: 'Jimmy'},
-                                {key: 'Julie'},
-                                {key: 'Devin'},
-                                {key: 'Dan'},
-                                {key: 'Dominic'},
-                                {key: 'Jackson'},
-                                {key: 'James'},
-                                {key: 'Joel'},
-                                {key: 'John'},
-                                {key: 'Jillian'},
-                                {key: 'Jimmy'},
-                                {key: 'Julie'},
-                            ]}
+                            data={this.state.movies}
                             renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
                         />
                     </View>
